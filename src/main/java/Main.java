c118f6935a2ae37b6a5c361938ecb3d39ddabbdf
dpwl0974 <dpwl0974@gmail.com>
2025-08-11 22:02:39 +0900

    feat: 모니터가 아닌 저장소로 출력 데이터 연결

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,40 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        experiment1();
+        experiment2();
     }
 
+    //출력을 모니터가 아닌 내 저장소로 연결
+    //그럼 꺼내쓸 수 있음 출력
+    public static void experiment2() {
+        System.out.println("Hello World!");
+
+        PrintStream originalOut = System.out; //백업용
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+
+        System.setOut(printStream);
+
+        System.out.println("하하");
+
+        String outStr = outputStream.toString();
+        System.setOut(originalOut); //모니터로 데이터 보내므로 보임
+        printStream.close();
+
+        if(outStr.equals("Hello World!")){
+            System.out.println("일치");
+        } else{
+            System.out.println("불일치");
+        }
+
+        System.out.println(outStr); //저장소로 데이터 보내므로 안보임
+
+
+    }
 
     public static void experiment1() {
 
