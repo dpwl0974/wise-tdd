3c81d453cd31457c79acc6274a9c4cc6aa956cdb
dpwl0974 <dpwl0974@gmail.com>
2025-08-12 19:22:17 +0900

    chore: AppTestRunner 주석추가

--- src/test/java/com/back/AppTestRunner.java
+++ src/test/java/com/back/AppTestRunner.java
@@ -5,6 +5,7 @@ import com.back.standard.util.TestUtil;
 import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
+// 계속 실행되는, 필요한 기능 클래스화
 public class AppTestRunner {
 
     public static String run(String input) {
