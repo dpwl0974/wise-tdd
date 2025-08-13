77d595d6190549323bd6fc37822bbf239cf4d3a8
dpwl0974 <dpwl0974@gmail.com>
2025-08-13 14:49:18 +0900

    feat: 9단계 json 2개 저장 red

--- src/test/java/com/back/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
+++ src/test/java/com/back/domain/wiseSaying/repository/WiseSayingFileRepositoryTest.java
@@ -23,7 +23,24 @@ public class WiseSayingFileRepositoryTest {
         WiseSaying wiseSaying = new WiseSaying("rksk","rksjk");
 
         wiseSayingFileRepository.save(wiseSaying);
+
         WiseSaying foundwiseSaying = wiseSayingFileRepository.FindByIdOrNull(1);
         assertThat(foundwiseSaying).isEqualTo(wiseSaying);
+    }
+
+    @Test
+    @DisplayName("2개 이상 명언 저장")
+    void t2() {
+        WiseSaying wiseSaying1 = new WiseSaying("하하","호호");
+        WiseSaying wiseSaying2 = new WiseSaying("흐흐","히히");
+
+        wiseSayingFileRepository.save(wiseSaying1);
+        wiseSayingFileRepository.save(wiseSaying2);
+
+        WiseSaying foundwiseSaying1 = wiseSayingFileRepository.FindByIdOrNull(1);
+        assertThat(foundwiseSaying1).isEqualTo(wiseSaying1);
+
+        WiseSaying foundwiseSaying2 = wiseSayingFileRepository.FindByIdOrNull(2);
+        assertThat(foundwiseSaying2).isEqualTo(wiseSaying2);
     }
 }
